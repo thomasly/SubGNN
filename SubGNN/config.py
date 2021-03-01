db7b3df3f19ae1ae58d7b93cbb6a73247faabc09
Yang Liu <thomasliuy@gmail.com>
2021-03-01 16:15:19 -0500

    move config.py to SubGNN directory

---
+++ SubGNN/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# directory where data and results will be stored
+# TODO: *UPDATE TO YOUR DIRECTORY OF CHOICE*
+PROJECT_ROOT = Path("/raid/home/public/YangLiu/SubGNN_data")
+
+# padding
+PAD_VALUE = 0
